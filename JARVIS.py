3270f34ea2d60180d988c1312a01d974972ff7b1
Aaron Santa Cruz <aaron.santacruz03@gmail.com>
2018-09-22 13:27:42 -0600

    added requirements.txt and weather to commands

--- JARVIS.py
+++ JARVIS.py
@@ -1,15 +1,27 @@
 from gtts import gTTS
 import speech_recognition as sr
 import os
+import sys
+import pyowm
 import re
 import webbrowser
 import smtplib
-import requests
+import urllib.request
+from geopy.geocoders import Nominatim
+from datetime import date, timedelta
 from weather import Weather
+from random import randint
+import pyttsx3
+import forecastio
+import json
 
+owm = pyowm.OWM('fca9e4262edac35767e09e053c4c76d0')
+engine = pyttsx3.init()
+geolocator = Nominatim(user_agent="JARVIS2")
 def talkToMe(audio):
     "speaks audio passed as argument"
-
+    engine.say(audio)
+    engine.runAndWait()
     print(audio)
     for line in audio.splitlines():
         os.system("say " + audio)
@@ -34,11 +46,15 @@ def myCommand():
     try:
         command = r.recognize_google(audio).lower()
         print('You said: ' + command + '\n')
-
+        engine.say('You said: ' + command)
+        engine.runAndWait()
     #loop back to continue to listen for commands if unrecognizable speech is received
     except sr.UnknownValueError:
         print('Your last command couldn\'t be heard')
+        engine.say('Please repeat what you said.')
+        engine.runAndWait()
         command = myCommand();
+
 
     return command
 
@@ -46,27 +62,21 @@ def myCommand():
 def assistant(command):
     "if statements for executing commands"
 
-    if 'open reddit' in command:
-        reg_ex = re.search('open reddit (.*)', command)
-        url = 'https://www.reddit.com/'
-        if reg_ex:
-            subreddit = reg_ex.group(1)
-            url = url + 'r/' + subreddit
-        webbrowser.open(url)
-        print('Done!')
-
-    elif 'open website' in command:
+    if 'open website' in command:
         reg_ex = re.search('open website (.+)', command)
         if reg_ex:
             domain = reg_ex.group(1)
             url = 'https://www.' + domain
             webbrowser.open(url)
             print('Done!')
+            engine.say('Opening ' + url)
+            engine.runAndWait()
         else:
             pass
 
     elif 'what\'s up' in command:
         talkToMe('Just doing my thing')
+
     elif 'joke' in command:
         res = requests.get(
                 'https://icanhazdadjoke.com/',
@@ -74,6 +84,8 @@ def assistant(command):
                 )
         if res.status_code == requests.codes.ok:
             talkToMe(str(res.json()['joke']))
+            engine.say(str(res.json()['joke']))
+            engine.runAndWait()
         else:
             talkToMe('oops!I ran out of jokes')
 
@@ -82,9 +94,15 @@ def assistant(command):
         if reg_ex:
             city = reg_ex.group(1)
             weather = Weather()
-            location = weather.lookup_by_location(city)
-            condition = location.condition()
-            talkToMe('The Current weather in %s is %s The tempeture is %.1f degree' % (city, condition.text(), (int(condition.temp())-32)/1.8))
+            location = geolocator.geocode(city)
+            placeweather = location.latitude, location.longitude
+            #Get weather
+            observation = owm.weather_at_place(city)
+            w = observation.get_weather()
+            currentTemperature = w.get_temperature('celsius')
+            talkToMe(currentTemperature['temp'])
+            print (currentTemperature['temp'])
+
 
     elif 'weather forecast in' in command:
         reg_ex = re.search('weather forecast in (.*)', command)
