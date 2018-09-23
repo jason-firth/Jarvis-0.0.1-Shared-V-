0447a02b936aef4968d22bfe5e22af89d6b9e5b6
Aaron Santa Cruz <aaron.santacruz03@gmail.com>
2018-09-23 09:36:00 -0600

    Made Jarvis more Human Like
    - Made it say things that a human would say
    -Made it always listen

--- JARVIS.py
+++ JARVIS.py
@@ -27,10 +27,10 @@ Currenthour=now.strftime('%I')
 AmPm=now.strftime('%p')
 
 if(int(Currenthour)<=12 and AmPm =='Pm'):
-    engine.say('Good evening sir')
+    engine.say('Good evening sir, How can I help?')
     engine.runAndWait()
 else:
-    engine.say('Good morning sir')
+    engine.say('Good morning sir, How can I help?')
     engine.runAndWait()
 
 
@@ -122,13 +122,11 @@ def myCommand():
     try:
         command = r.recognize_google(audio).lower()
         print('You said: ' + command + '\n')
-        engine.say('You said: ' + command)
-        engine.runAndWait()
+        # engine.say('You said: ' + command)
+        # engine.runAndWait()
     #loop back to continue to listen for commands if unrecognizable speech is received
     except sr.UnknownValueError:
         print('Your last command couldn\'t be heard')
-        engine.say('Please repeat what you said.')
-        engine.runAndWait()
         command = myCommand();
 
 
@@ -214,8 +212,8 @@ def assistant(command):
             else:
                 talkToMe('oops!I ran out of jokes')
 
-        elif 'current weather in' in command:
-            reg_ex = re.search('current weather in (.*)', command)
+        elif 'weather in' in command:
+            reg_ex = re.search('weather in (.*)', command)
             if reg_ex:
                 city = reg_ex.group(1)
                 weather = Weather()
@@ -225,12 +223,15 @@ def assistant(command):
                 observation = owm.weather_at_place(city)
                 w = observation.get_weather()
                 currentTemperature = w.get_temperature('celsius')
-                talkToMe(currentTemperature['temp'])
-                print (currentTemperature['temp'])
+                tempCelsius = int(currentTemperature['temp']) * 1.8
+                tempFahrenheit = tempCelsius + 32
+                tempFahrenheit = str(tempFahrenheit)
+                talkToMe("It is "+tempFahrenheit + " degrees Fahrenheit")
+                print ("It is "+tempFahrenheit + " degrees Fahrenheit")
 
 
         elif 'weather forecast in' in command:
-            reg_ex = re.search('weather forecast in (.*)', command)
+            reg_ex = re.search('weather forecast(.*)', command)
             if reg_ex:
                 city = reg_ex.group(1)
                 weather = Weather()
@@ -239,35 +240,6 @@ def assistant(command):
                 for i in range(0,3):
                     talkToMe('On %s will it %s. The maximum temperture will be %.1f degree.'
                              'The lowest temperature will be %.1f degrees.' % (forecasts[i].date(), forecasts[i].text(), (int(forecasts[i].high())-32)/1.8, (int(forecasts[i].low())-32)/1.8))
-
-
-        elif 'email' in command:
-            talkToMe('Who is the recipient?')
-            recipient = myCommand()
-
-            if 'John' in recipient:
-                talkToMe('What should I say?')
-                content = myCommand()
-
-                #init gmail SMTP
-                mail = smtplib.SMTP('smtp.gmail.com', 587)
-
-                #identify to server
-                mail.ehlo()
-
-                #encrypt session
-                mail.starttls()
-
-                #login
-                mail.login('username', 'password')
-
-                #send message
-                mail.sendmail('John Fisher', 'JARVIS2.0@protonmail.com', content)
-
-                #end mail connection
-                mail.close()
-
-                talkToMe('Email sent.')
 
             else:
                 talkToMe('I don\'t know what you mean!')
