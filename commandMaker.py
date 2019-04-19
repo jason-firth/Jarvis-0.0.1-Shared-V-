83bd0bad25893f813cb7a25574446eee5ca3e469
21sanaar <21sanaar@mcps.k12.mt.us>
2019-04-19 17:18:37 -0600

    Added A Simple Command Maker

--- commandMaker.py
+++ commandMaker.py
@@ -1,3 +1,8 @@
-print("Simple Command Maker")
+import os
+print("Simple Jarvis Command Maker")
+
+''' Respond Only Commands '''
 
 checkFor = input("What do you want Jarvis to check For?: ")
+returnCom = input("What do you want Jarvis to say?: ")
+os.system(''' echo '\tif ''' + '''"''' +  str(checkFor) + '''"''' + ''' in command: \t return("''' + str(returnCom) + '''") \n' >> commands.py ''')
--- commands.py
+++ commands.py
@@ -34,184 +34,184 @@ If you want Jarvis to say something, return "(What you want jarvis to say)"
 '''
 
 def checkCommand(command, ser, moviePlaying, player, paused):
-    if 'time' in command:
-        now = datetime.now()
-        return("It is " + now.strftime('%I:%M %p'))
+	if 'time' in command:
+		now = datetime.now()
+		return("It is " + now.strftime('%I:%M %p'))
 
-    #get date
-    elif 'date' in command:
-        month = "";
-        now=datetime.now()
-        if(strftime("%m") == "01"):
-            month = "January"
-        elif(strftime("%m") == "02"):
-            month = "Febuary"
-        elif(strftime("%m") == "03"):
-            month = "March"
-        elif(strftime("%m") == "04"):
-            month = "April"
-        elif(strftime("%m") == "05"):
-            month = "May"
-        elif(strftime("%m")== "06"):
-            month = "June"
-        elif(strftime("%m") == "07"):
-            month = "July"
-        elif(strftime("%m") == "08"):
-            month = "August"
-        elif(strftime("%m") == "09"):
-            month = "September"
-        elif(strftime("%m") == "10"):
-            month = "October"
-        elif(strftime("%m") == "11"):
-            month = "November"
-        else:
-            month = "December"
-        if(strftime("%d")=="1"):
-            return("The date is "+ month + " 1st, " + strftime("%Y"))
-        elif(strftime("%d")=="2"):
-            return("The date is "+ month + " 2nd, " + strftime("%Y"))
-        elif(strftime("%d")=="3"):
-            return("The date is "+ month + " 3rd, " + strftime("%Y"))
-        elif(strftime("%d")=="21"):
-            return("The date is "+ month + " 21st, " + strftime("%Y"))
-        elif(strftime("%d")=="22"):
-            return("The date is "+ month + " 22nd, " + strftime("%Y"))
-        elif(strftime("%d")=="23"):
-            return("The date is "+ month + " 23rd, " + strftime("%Y"))
-        else:
-            return("The date is " + month + strftime("%d") + ", " + strftime("%Y"))
-    elif 'what\'s up' in command:
-        return('Just doing my thing')
-    elif 'help' in command:
-        return('I can inform you of the time, tell you the date, and play entertainment for you')
-    #elif 'joke' in command:
-        #res = requests.get(
-        #		'https://icanhazdadjoke.com/',
-        #		headers={"Accept":"application/json"}
-        #		)
-        #if res.status_code == requests.codes.ok:
-            #return(str(res.json()['joke']))
-            #engine.say(str(res.json()['joke']))
-            #engine.runAndWait()
-        #else:
-            #return('oops! I ran out of jokes')
+	#get date
+	elif 'date' in command:
+		month = "";
+		now=datetime.now()
+		if(strftime("%m") == "01"):
+			month = "January"
+		elif(strftime("%m") == "02"):
+			month = "Febuary"
+		elif(strftime("%m") == "03"):
+			month = "March"
+		elif(strftime("%m") == "04"):
+			month = "April"
+		elif(strftime("%m") == "05"):
+			month = "May"
+		elif(strftime("%m")== "06"):
+			month = "June"
+		elif(strftime("%m") == "07"):
+			month = "July"
+		elif(strftime("%m") == "08"):
+			month = "August"
+		elif(strftime("%m") == "09"):
+			month = "September"
+		elif(strftime("%m") == "10"):
+			month = "October"
+		elif(strftime("%m") == "11"):
+			month = "November"
+		else:
+			month = "December"
+		if(strftime("%d")=="1"):
+			return("The date is "+ month + " 1st, " + strftime("%Y"))
+		elif(strftime("%d")=="2"):
+			return("The date is "+ month + " 2nd, " + strftime("%Y"))
+		elif(strftime("%d")=="3"):
+			return("The date is "+ month + " 3rd, " + strftime("%Y"))
+		elif(strftime("%d")=="21"):
+			return("The date is "+ month + " 21st, " + strftime("%Y"))
+		elif(strftime("%d")=="22"):
+			return("The date is "+ month + " 22nd, " + strftime("%Y"))
+		elif(strftime("%d")=="23"):
+			return("The date is "+ month + " 23rd, " + strftime("%Y"))
+		else:
+			return("The date is " + month + strftime("%d") + ", " + strftime("%Y"))
+	elif 'what\'s up' in command:
+		return('Just doing my thing')
+	elif 'help' in command:
+		return('I can inform you of the time, tell you the date, and play entertainment for you')
+	#elif 'joke' in command:
+		#res = requests.get(
+		#		'https://icanhazdadjoke.com/',
+		#		headers={"Accept":"application/json"}
+		#		)
+		#if res.status_code == requests.codes.ok:
+			#return(str(res.json()['joke']))
+			#engine.say(str(res.json()['joke']))
+			#engine.runAndWait()
+		#else:
+			#return('oops! I ran out of jokes')
 
-    # elif ' in' in command:
-    #     reg_ex = re.search('weather in (.*)', command)
-    #     if reg_ex:
-    #         city = reg_ex.group(1)
-    #         weather = Weather()
-    #         location = geolocator.geocode(city)
-    #         placeweather = location.latitude, location.longitude
-    #         #Get weather
-    #         observation = owm.weather_at_place(city)
-    #         w = observation.get_weather()
-    #         currentTemperature = w.get_temperature('celsius')
-    #         tempCelsius = int(currentTemperature['temp']) * 1.8
-    #         tempFahrenheit = tempCelsius + 32
-    #         tempFahrenheit = str(tempFahrenheit)
-    #         return("It is "+tempFahrenheit + " degrees Fahrenheit")
-    #         print ("It is "+tempFahrenheit + " degrees Fahrenheit")
+	# elif ' in' in command:
+	#     reg_ex = re.search('weather in (.*)', command)
+	#     if reg_ex:
+	#         city = reg_ex.group(1)
+	#         weather = Weather()
+	#         location = geolocator.geocode(city)
+	#         placeweather = location.latitude, location.longitude
+	#         #Get weather
+	#         observation = owm.weather_at_place(city)
+	#         w = observation.get_weather()
+	#         currentTemperature = w.get_temperature('celsius')
+	#         tempCelsius = int(currentTemperature['temp']) * 1.8
+	#         tempFahrenheit = tempCelsius + 32
+	#         tempFahrenheit = str(tempFahrenheit)
+	#         return("It is "+tempFahrenheit + " degrees Fahrenheit")
+	#         print ("It is "+tempFahrenheit + " degrees Fahrenheit")
 
-    #elif 'lockdown in command:
+	#elif 'lockdown in command:
 
-    elif 'dandy\'s favorite' in command:
-        return('Dandy\'s favorite student is Jason. He also likes Aaron equally as much.')
+	elif 'dandy\'s favorite' in command:
+		return('Dandy\'s favorite student is Jason. He also likes Aaron equally as much.')
 
-    elif 'play' in command:
-        movieInput = command.split("play")[1].replace(" ", "")
-        directory = "/media/pi/8891-D645/"
-        movies = {}
-        moviesList = []
-        filetypes = ["mp4","mp3", "m4v"]
-        
-        for movie in os.listdir(directory):
-            if("." in movie):
-                if(movie.split(".")[1] in filetypes):
-                    pathToMovie = directory+movie
-                    movies[movie.split(".")[0]] = pathToMovie
-                    moviesList.append(movie.split(".")[0].replace("_", " ").replace(" ", ""))
+	elif 'play' in command:
+		movieInput = command.split("play")[1].replace(" ", "")
+		directory = "/media/pi/8891-D645/"
+		movies = {}
+		moviesList = []
+		filetypes = ["mp4","mp3", "m4v"]
+		
+		for movie in os.listdir(directory):
+			if("." in movie):
+				if(movie.split(".")[1] in filetypes):
+					pathToMovie = directory+movie
+					movies[movie.split(".")[0]] = pathToMovie
+					moviesList.append(movie.split(".")[0].replace("_", " ").replace(" ", ""))
 
-                    print("Movie: " + movie.split(".")[0] + " Location: " + pathToMovie)
+					print("Movie: " + movie.split(".")[0] + " Location: " + pathToMovie)
 
-        print(movies)
-        # FOR JARVIS: change movieToPlay to command.split("play")[1]
-        movieToPlay = movieInput
-        if(movieToPlay in moviesList):
-            # os.system("vlc " + movies[movieToPlay] + " --fullscreen --play-and-exit")
-            player = vlc.MediaPlayer(movies[movieToPlay])
+		print(movies)
+		# FOR JARVIS: change movieToPlay to command.split("play")[1]
+		movieToPlay = movieInput
+		if(movieToPlay in moviesList):
+			# os.system("vlc " + movies[movieToPlay] + " --fullscreen --play-and-exit")
+			player = vlc.MediaPlayer(movies[movieToPlay])
 
-            player.set_fullscreen(True)
-            player.video_set_mouse_input(True)
-            player.video_set_key_input(True)
-            os.system("")
-            player.play()
-            moviePlaying = True
-            return("command no voice")
-        else:
-            return("That does not exist")
-    elif 'pause' in command:
-        if(not paused and moviePlaying):
-            player.pause()
-            paused = True
-            return("command no voice")
-        else:
-            return("Nothing is playing")
-    elif 'quit' in command:
-        if(moviePlaying):
-            player.stop()
-            moviePlaying = False
-            return("command no voice")
-        else:
-            return("Nothing is playing")
-    elif 'resume' in command:
-        if(moviePlaying and paused):
-            player.play()
-            paused = False
-            moviePlaying = True
-            return("command no voice")
-    elif 'volume to' in command:
-        setToPercent = command.split("volume to")[1]
-        os.system("amixer set 'PCM' " + setToPercent+"%")
-        return("command no voice")
-    elif 'go to the middle' in command:
-        if(moviePlaying):
-            player.set_position(0.5)
-            return("command no voice")
-        else:
-            return("Nothing is playing")
-    elif 'go to the start' in command or 'go the beginning' in command:
-        if(moviePlaying):
-            player.set_position(0.2)
-            return("command no voice")
-        else:
-            return("Nothing is playing")
-    elif 'go to the end' in command:
-        if(moviePlaying):
-            player.set_position(0.9)
-            return("command no voice")
-        else:
-            return("Nothing is playing")
-    elif 'go to' in command:
-        if(moviePlaying):
-            position = command.split("go to ")[1]
-            player.set_position((int(position)/100))
-            return("command no voice")
-        else:
-            return("Nothing is playing")
-    elif 'color to' in command:
-        color = command.split("color to")[1]
-        ser.write(color.encode())
-        return("Changing Color")
-        # assistant(myCommand())
-    # elif 'start hud' in command:
-    # 	starthud()
-    # elif 'stop hud' in command:
-    # 	stophud()
+			player.set_fullscreen(True)
+			player.video_set_mouse_input(True)
+			player.video_set_key_input(True)
+			os.system("")
+			player.play()
+			moviePlaying = True
+			return("command no voice")
+		else:
+			return("That does not exist")
+	elif 'pause' in command:
+		if(not paused and moviePlaying):
+			player.pause()
+			paused = True
+			return("command no voice")
+		else:
+			return("Nothing is playing")
+	elif 'quit' in command:
+		if(moviePlaying):
+			player.stop()
+			moviePlaying = False
+			return("command no voice")
+		else:
+			return("Nothing is playing")
+	elif 'resume' in command:
+		if(moviePlaying and paused):
+			player.play()
+			paused = False
+			moviePlaying = True
+			return("command no voice")
+	elif 'volume to' in command:
+		setToPercent = command.split("volume to")[1]
+		os.system("amixer set 'PCM' " + setToPercent+"%")
+		return("command no voice")
+	elif 'go to the middle' in command:
+		if(moviePlaying):
+			player.set_position(0.5)
+			return("command no voice")
+		else:
+			return("Nothing is playing")
+	elif 'go to the start' in command or 'go the beginning' in command:
+		if(moviePlaying):
+			player.set_position(0.2)
+			return("command no voice")
+		else:
+			return("Nothing is playing")
+	elif 'go to the end' in command:
+		if(moviePlaying):
+			player.set_position(0.9)
+			return("command no voice")
+		else:
+			return("Nothing is playing")
+	elif 'go to' in command:
+		if(moviePlaying):
+			position = command.split("go to ")[1]
+			player.set_position((int(position)/100))
+			return("command no voice")
+		else:
+			return("Nothing is playing")
+	elif 'color to' in command:
+		color = command.split("color to")[1]
+		ser.write(color.encode())
+		return("Changing Color")
+		# assistant(myCommand())
+	# elif 'start hud' in command:
+	# 	starthud()
+	# elif 'stop hud' in command:
+	# 	stophud()
 
-    
-    # elif 'stop hud' in command:
-    # 	stophud()
-    # elif 'start hud' in command:
-    # 	os.system("python3 hud.py")
-    
+	
+	# elif 'stop hud' in command:
+	# 	stophud()
+	# elif 'start hud' in command:
+	# 	os.system("python3 hud.py")
+
